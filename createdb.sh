3a423f940bfe4be7d988df53f28f61f4dfd2eebc
Mourad Khayati <mkhayati@exascale.info>
2020-03-29 20:31:09 +0200

    createdb

---
+++ createdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+monetdbd stop monetdb_farm
+rm -rf monetdb_farm/
+monetdbd create monetdb_farm
+monetdbd start monetdb_farm
+monetdb create testdb
+monetdb release testdb
+monetdb set embedpy=yes testdb
+sed -i "s#root_folder#$(pwd)#g"  ../decomp_udf.sql
+sed -i "s#root_folder#$(pwd)#g" ../recov_udf.sql
