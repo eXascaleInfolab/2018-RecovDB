b47eea5414765de6e6b3f706fd62eeecd4a2cc9f
Mourad Khayati <mkhayati@exascale.info>
2020-03-29 18:24:18 +0200

    Update decomp_udf.sql

--- decomp_udf.sql
+++ decomp_udf.sql
@@ -34,5 +34,5 @@ CREATE FUNCTION centroid_decomposition(x1 float, x2 float, x3 float, x4 float) R
 
 DROP TABLE time_series;
 CREATE TABLE time_series(x1 float, x2 float, x3 float, x4 float);
-COPY 1000 RECORDS INTO time_series FROM 'PATH/decomp/src/climate.csv' USING DELIMITERS ',','\n';
+COPY 1000 RECORDS INTO time_series FROM 'root_folder/decomp/src/climate.csv' USING DELIMITERS ',','\n';
 SELECT centroid_decomposition(x1, x2, x3, x4) AS result FROM time_series;
