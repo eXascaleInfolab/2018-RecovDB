2bf9e3ce084d86648f54e3eacc88c8a7a06b1503
Mourad Khayati <mkhayati@exascale.info>
2020-03-30 15:55:37 +0200

    Update monetdb_install.sh

--- monetdb_install.sh
+++ monetdb_install.sh
@@ -1,7 +1,7 @@
 sudo touch /etc/apt/sources.list.d/monetdb.list
 sudo chmod 777 /etc/apt/sources.list.d/monetdb.list
-echo 'deb https://dev.monetdb.org/downloads/deb/ $(lsb_release -cs) monetdb' >> /etc/apt/sources.list.d/monetdb.list
-echo 'deb-src https://dev.monetdb.org/downloads/deb/ $(lsb_release -cs) monetdb' >> /etc/apt/sources.list.d/monetdb.list
+echo "deb https://dev.monetdb.org/downloads/deb/ $(lsb_release -cs) monetdb" >> /etc/apt/sources.list.d/monetdb.list
+echo "deb-src https://dev.monetdb.org/downloads/deb/ $(lsb_release -cs) monetdb" >> /etc/apt/sources.list.d/monetdb.list
 wget --output-document=- https://www.monetdb.org/downloads/MonetDB-GPG-KEY | sudo apt-key add -
 sudo apt update
 wget https://www.monetdb.org/downloads/sources/Nov2019-SP3/MonetDB-11.35.19.zip 
